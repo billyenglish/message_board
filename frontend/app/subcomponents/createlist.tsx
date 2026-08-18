b4d0e206ee48b2c2c0e41404e25c905ac337840d
Demar English <demarenglish@Demars-MacBook-Pro.local>
2026-08-18 16:29:50 -0400

    Created resuable list component.

---
+++ frontend/app/subcomponents/createlist.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+type CreateListProps ={
+    items: string;
+    id: number;
+}
+
+export default function CreateList({
+    items,
+    id,
+}:CreateListProps) {
+    return (
+        <ul
+            className="
+                flex
+                flex-col
+                gap-8
+            "
+        >
+            <li
+                key={id}
+                className="
+                    list-disc
+                "
+            >
+                {items}
+            </li>
+        </ul>
+    )
+}
