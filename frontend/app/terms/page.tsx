3aae460875956af35f10b4f9bf342bcd7b793fd6
Demar English <demarenglish@Demars-MacBook-Pro.local>
2026-08-18 13:43:08 -0400

    Created terms page.

---
+++ frontend/app/terms/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Nav from "../components/navigation";
+import Footer from "../components/footer";
+
+export default function Terms() {
+
+    return (
+        <section>
+            <Nav />
+            <h2>
+                Terms
+            </h2>
+            <Footer />
+        </section>
+    );
+};
+
