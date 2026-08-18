23db6b5fd0516260454b543f14824768e7835549
Demar English <demarenglish@Demars-MacBook-Pro.local>
2026-08-18 16:05:05 -0400

    Created terms content for pages.

--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 import Nav from "../components/navigation";
 import Footer from "../components/footer";
+import ThemeContext from "../currenttheme/themecontext";
+import { useContext } from "react";
 
 export default function Terms() {
 
+    const { toggleTheme } = useContext(ThemeContext);
+
     return (
-        <section>
+        <section
+            className={`
+                min-h-screen
+                min-w-screen
+                ${  toggleTheme
+                    ? "bg-(--dark-background) text-(--primary-dark-text"
+                    : "bg-(--light-backround)"
+                }
+        `}>
             <Nav />
             <h2>
                 Terms
